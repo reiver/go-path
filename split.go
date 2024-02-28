787190e321d490f07d08f589a7e7e69de99e1d6e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-28 04:04:12 -0800

    path.Split

--- split.go
+++ split.go
@@ -6,6 +6,14 @@ import (
 
 // Split splits a path into the directory part and file part.
 // It does NOT do any clean-up (such as removing the trailing slash on the directory).
+//
+// Note that whether there is a trailing separator (i.e., "/") character or not has an effect on the result.
+//
+// For example:
+//
+//	"/once/twice/thrice/fource"  -> dir: "/once/twice/thrice/",        file: "fource"
+//
+//	"/once/twice/thrice/fource/" -> dir: "/once/twice/thrice/fource/", file: ""
 func Split(path string) (dir string, file string) {
 
 	index := strings.LastIndexByte(path, '/')
