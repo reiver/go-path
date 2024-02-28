b9c32960e1c0d6e656f0ed9b738631911ffd757b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-27 20:27:02 -0800

    path.Ext

--- ext.go
+++ ext.go
@@ -1,48 +1,6 @@
 package path
 
-// Ext return the file-name extension if it exists, and return an empty string otherwise.
-//
-// Some examples:
-//
-//	"" → ""
-//
-//	"/" → ""
-//
-//	"/doc.txt" → ".txt"
-//
-//	"doc.txt" → ".txt"
-//
-//	"/image.jpeg" → ".jpeg"
-//
-//	"image.jpeg" → ".jpeg"
-//
-//	"/apple/banana/cherry.html" → ".html"
-//
-//	"apple/banana/cherry.html" → ".html"
-//
-//	"/path/to/archive.tar.gz" → ".gz"
-//
-//	"path/to/archive.tar.gz" → ".gz"
-//
-//	"/once/twice/thrice/fource" → ""
-//
-//	"once/twice/thrice/fource" → ""
-//
-//	"/LICENSE" → ""
-//
-//	"LICENSE" → ""
-//
-//	"/src/README.md" → ".md"
-//
-//	"src/README.md" → ".md"
-//
-//	"/ALLCAPS.HTML" → ".HTML"
-//
-//	"ALLCAPS.HTML" → ".HTML"
-//
-//	"/something/name.dir/filename" → ""
-//
-//	"something/name.dir/filename" → ""
+// Ext returns the file-name extension if it exists, and return an empty string otherwise.
 func Ext(path string) string {
 
 	var length int = len(path)
